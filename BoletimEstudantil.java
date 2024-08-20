8089a831b2bb9c1ad50ecef2c20241b42d820201
Rafaela Mariano Sanchez Amiliátegui <amiliateguisanchez@gmail.com>
2024-08-20 18:08:53 -0300

    Add files via upload

---
+++ BoletimEstudantil.java
@@ -0,0 +1,13 @@
+public class BoletimEstudantil {
+    public static void main(String[] args) {
+        int mediFinal = 6;
+        if(mediFinal<6)
+            System.out.println("REPROVADO");
+        else if(mediFinal==6)
+            System.out.println("PROVA MINERVA");
+        else
+            System.out.println("APROVADO");
+
+
+    }
+}
