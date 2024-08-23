d26caeae477f38f934fdeeb13e4b28bc4a27fb73
Rafaela Mariano Sanchez Amiliátegui <amiliateguisanchez@gmail.com>
2024-08-23 09:31:15 -0300

    Add files via upload

---
+++ Operadores2.java
@@ -0,0 +1,18 @@
+//classe operadores.java
+public class Operadores2 {
+    public static void main(String[] args)  {
+        int a, b;
+
+        a = 5;
+        b = 6;
+        String resultado = a==b ?"Verdadeiro" : "Falso";//pode ser usado assim tambem
+    /*EXEMPLO DE CONDICIONAL UTILIZANDO UMA ESTRUTURA IF/ELSE */
+
+        if(a==b)
+            resultado = "Verdadeiro";
+        else 
+            resultado = "Falso";
+
+        System.out.println(resultado);
+        } 
+}
